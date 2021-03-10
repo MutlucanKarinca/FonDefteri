2d86596ef0a2bd2397fe2ff5bff8d9b5a5a979c4
Mutlucan Karınca <mutlucankarinca@gmail.com>
2021-03-10 22:39:19 +0300

    User ve auth işlemleri yapıldı

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,7 +1,19 @@
-﻿namespace Business.Abstract
+﻿using System.Collections.Generic;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+
+namespace Business.Abstract
 {
     public interface IUserService
     {
-        
+        IDataResult<List<User>> GetAll();
+        IDataResult<User> GetById(int id);
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Delete(User user);
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
+
+
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -3,10 +3,14 @@
     public static class Messages
     {
         public static string FundAdded = "Fon Eklendi";
-
         public static string FundDeleted = "Fon Silindi";
         public static string FundIdInvalid = "Bulunamadı";
-
         public static string FundUpdated = "Fon Güncellendi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
